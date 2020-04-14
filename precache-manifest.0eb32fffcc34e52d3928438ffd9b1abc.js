1bab80c2b84d990c463f2efe520148c29317d88b
Jessan Cherian <jessan.cherian@gmail.com>
2020-04-14 00:50:46 -0700

    Updates

---
+++ precache-manifest.0eb32fffcc34e52d3928438ffd9b1abc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98db970f4c275593a2c7036fb64f374d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "649937130062b66d558a",
+    "url": "/static/css/main.31fb0c45.chunk.css"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649937130062b66d558a",
+    "url": "/static/js/main.1e56b446.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
